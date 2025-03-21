ab691d1fffe292803a98ead74be7d35e3673439f
kumaraswini-11 <kumaraswini.11.11@gmail.com>
2025-03-21 23:26:29 +0530

    Add shadcn/ui components and apply custom styling

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,15 +1,17 @@
 import type {Metadata} from "next";
-import {Geist, Geist_Mono} from "next/font/google";
+import {DM_Sans, Barlow} from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
+const dmSans = DM_Sans({
+  variable: "--font-dmSans",
   subsets: ["latin"],
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
 });
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
+const barlow = Barlow({
+  variable: "--font-barlow",
   subsets: ["latin"],
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
 });
 
 export const metadata: Metadata = {
@@ -57,7 +59,7 @@ export default function RootLayout({
     <html
       lang="en"
       suppressHydrationWarning>
-      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>{children}</body>
+      <body className={`${dmSans.variable} ${barlow.variable} antialiased`}>{children}</body>
     </html>
   );
 }
